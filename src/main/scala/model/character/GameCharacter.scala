250a108f324ef85fe9d9bf1a7cc9dc3ee9eb9d54
R8V <ignacio.slater@ug.uchile.cl>
2022-08-28 19:27:04 -0400

    Init

---
+++ src/main/scala/model/character/GameCharacter.scala
@@ -0,0 +1,13 @@
+/*
+ * "final-reality-scala" (c) by Ignacio Slater M.
+ * "final-reality-scala" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+package cl.uchile.dcc.finalreality
+package model.character
+
+trait GameCharacter {
+
+}
